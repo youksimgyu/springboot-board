bfcc5229504d098c7cf1e1d3de9d50e3a58ccba5
youksimgyu <dbtlarb@gmail.com>
2022-11-28 16:42:54 +0900

    #1 - Data REST 설정
    Restful 하게 만들게끔 설정

--- springboot-board/src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
+++ springboot-board/src/main/java/com/example/springbootboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package com.example.springbootboard.repository;
 
 import com.example.springbootboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
