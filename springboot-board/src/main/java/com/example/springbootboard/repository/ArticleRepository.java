f6afa7850fbbac0a499c2ff8259f551bc5c3050d
youksimgyu <dbtlarb@gmail.com>
2022-12-01 17:35:24 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- springboot-board/src/main/java/com/example/springbootboard/repository/ArticleRepository.java
+++ springboot-board/src/main/java/com/example/springbootboard/repository/ArticleRepository.java
@@ -1,11 +1,28 @@
 package com.example.springbootboard.repository;
 
 import com.example.springbootboard.domain.Article;
+import com.example.springbootboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
-
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>, JpaSpecificationExecutor<Article>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
