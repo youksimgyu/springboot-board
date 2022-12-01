6707846b7ea97b3b6c7ea7fe087840b18940e624
youksimgyu <dbtlarb@gmail.com>
2022-12-01 18:50:42 +0900

    #2 - 게시글 관련 view 테스트 작성
    우선 어떤 경로로 열어줄지 테스트로 명세를 뽑는다

---
+++ springboot-board/src/main/java/com/example/springbootboard/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package com.example.springbootboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * /articles
+ * /articles/{article_id}
+ * /articles/search
+ * /articles/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
