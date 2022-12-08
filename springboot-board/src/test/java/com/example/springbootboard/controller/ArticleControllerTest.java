3c230057ab1e6f1bca4418e289887f022a29aa9c
youksimgyu <dbtlarb@gmail.com>
2022-12-08 17:40:07 +0900

    #2 - contentTypeCompatibleWith 나머지도 추가

--- springboot-board/src/test/java/com/example/springbootboard/controller/ArticleControllerTest.java
+++ springboot-board/src/test/java/com/example/springbootboard/controller/ArticleControllerTest.java
@@ -44,7 +44,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(view().name("articleComments"))
                 .andExpect(model().attributeExists("article"));
@@ -59,7 +59,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -72,7 +72,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 
