ef59f97679f5354bc9c6f6b7269f1aa2cefa1b8a
youksimgyu <dbtlarb@gmail.com>
2022-12-08 17:39:18 +0900

    #2 - 게시판 뷰 기본 골격 작성
    컨트롤러가 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 구성
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 떄문에 수정

--- springboot-board/src/main/java/com/example/springbootboard/SpringBootBoardApplication.java
+++ springboot-board/src/main/java/com/example/springbootboard/SpringBootBoardApplication.java
@@ -2,12 +2,14 @@ package com.example.springbootboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
-public class SpringbootBoardApplication {
+public class SpringBootBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBoardApplication.class, args);
+		SpringApplication.run(SpringBootBoardApplication.class, args);
 
 		System.out.println("Hello World!");
 	}
--- springboot-board/src/test/java/com/example/springbootboard/controller/ArticleControllerTest.java
+++ springboot-board/src/test/java/com/example/springbootboard/controller/ArticleControllerTest.java
@@ -22,7 +22,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[View][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void given_when_then() throws Exception {
@@ -31,7 +30,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect((content().contentTypeCompatibleWith(MediaType.TEXT_HTML)))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
